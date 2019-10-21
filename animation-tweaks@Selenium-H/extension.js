52f6ecc3ccddce3faea7ee1b5c160b79cc04f291
D Paul <46006930+Selenium-H@users.noreply.github.com>
2019-10-22 01:03:17 +0530

    Worked on issue #8 , issue #4
    Added ability to manage application profiles. 
    Fixes minimize, unminimize and close animations with GNOME 3.34

--- animation-tweaks@Selenium-H/extension.js
+++ animation-tweaks@Selenium-H/extension.js
@@ -1,5 +1,6 @@
 /*
-Version 7
+
+Version 8
 =========
 
 Effect String Format   [ Status   Name   Tweens  IO      IW     IH     IPX     IPY         T     PPx     PPY     NO      NW      NH     NPX     NPY  ... ]
@@ -8,14 +9,15 @@ Read the effectParameters.txt File for details.
 
 */
 
-
 const Gio = imports.gi.Gio;
 const Lang = imports.lang;
 const Extension = imports.misc.extensionUtils.getCurrentExtension();
 const Convenience = Extension.imports.convenience;
+const Prefs = Extension.imports.prefs;
 const Meta = imports.gi.Meta;
 const Tweener = imports.ui.tweener;
 const Main = imports.ui.main;
+const Shell = imports.gi.Shell;
 
 let effectsManager = null;
 
@@ -24,12 +26,22 @@ function enable() {
   effectsManager = new EffectsManager();
   effectsManager.startEffectsManager();
   reloadExtensionOnPrefsChange();
+  reloadApplicationProfilesOnPrefsChange();
 
 }
 
 function disable() {
 
   effectsManager.destroy();
+
+}
+
+function reloadApplicationProfilesOnPrefsChange() {
+
+  // Reloads Application Profiles when preferences are changed.
+  effectsManager.prefs.connect("changed::reload-profiles-signal", () => {
+    effectsManager.loadProfilePrefs();
+  });
 
 }
 
@@ -44,6 +56,7 @@ function reloadExtensionOnPrefsChange() {
 }
 
 const EffectsManager = new Lang.Class({
+
   Name: "EffectsManager",
 
   _init: function () {
@@ -60,22 +73,22 @@ const EffectsManager = new Lang.Class({
       default :
         return;
       case Meta.WindowType.DROPDOWN_MENU :
-        eParams = this.prefs.get_strv("dropdown-menu-"+action );
+        eParams = this.getEffectFor("1","dropdownmenu",action);
         break;
       case Meta.WindowType.POPUP_MENU :
-        eParams = this.prefs.get_strv("popup-menu-"+action    );
+        eParams = this.getEffectFor("1","popupmenu",action);
         break;
       case Meta.WindowType.COMBO :
-        eParams = this.prefs.get_strv("combo-"+action         );
+        eParams = this.getEffectFor("1","combo",action);
         break;
       case Meta.WindowType.SPLASHSCREEN :
-        eParams = this.prefs.get_strv("splashscreen-"+action  );
+        eParams = this.getEffectFor("1","splashscreen",action);
         break;
       case Meta.WindowType.TOOLTIP :
-        eParams = this.prefs.get_strv("tooltip-"+action       );
+        eParams = this.getEffectFor("1","tooltip",action);
         break;
       case Meta.WindowType.OVERRIDE_OTHER :
-        eParams = this.prefs.get_strv("override-other-"+action);
+        eParams = this.getEffectFor("1","overrideother",action);
         break;
     }
 
@@ -90,37 +103,63 @@ const EffectsManager = new Lang.Class({
   },
 
   addWindowEffects : function (shellwm, actor, action) {
-
+        
     this.shellwm = shellwm;
     let eParams=[];
-
+    
     switch(actor.meta_window.window_type) {
       default :
         return;
       case Meta.WindowType.NORMAL :
-        eParams = this.prefs.get_strv("normal-"+action);
+        eParams = this.getEffectFor(Shell.WindowTracker.get_default().get_window_app(actor.meta_window).get_name(),"normal",action);
         break;
       case Meta.WindowType.DIALOG :
-        eParams = this.prefs.get_strv("dialog-"+action);
+        eParams = this.getEffectFor(Shell.WindowTracker.get_default().get_window_app(actor.meta_window).get_name(),"dialog",action);
         break;
       case Meta.WindowType.MODAL_DIALOG:
-        eParams = this.prefs.get_strv("modal-dialog-"+action);
+        eParams = this.getEffectFor(Shell.WindowTracker.get_default().get_window_app(actor.meta_window).get_name(),"modaldialog",action);
         break;
     }
 
     if(eParams[0] == "T") {
-
-      Tweener.removeTweens(actor);
-      if(action == "unminimize") {
-        if(Main.overview._shown) {
-          this.animationDone(actor,"unminimize");
-          return;
-        }
-        Main.wm._unminimizeWindowDone(this.shellwm ,actor);    	 
+   
+      switch(action) {
+      
+        case "open": 
+          Main.wm._removeEffect(Main.wm._mapping, actor);
+          break;
+          
+        case "close":
+          Main.wm._removeEffect(Main.wm._destroying, actor); 
+          
+          let window = actor.meta_window;
+          if (window.is_attached_dialog()) {
+            let parent = window.get_transient_for();
+            actor._parentDestroyId = parent.connect('unmanaged', () => {
+              Tweener.removeTweens(actor);
+              this.animationDone(actor,action);
+            });
+          }
+          break;
+          
+        case "minimize":
+          Main.wm._removeEffect(Main.wm._minimizing, actor);
+          break;
+          
+        case "unminimize":
+          if(Main.overview._shown) {
+            this.animationDone(actor,"unminimize");
+            return;
+          }
+          Main.wm._unminimizeWindowDone(this.shellwm ,actor);
+          break;
+          
       }
+
       let [success, geom] = actor.meta_window.get_icon_geometry();
       this.initParametersWindow(actor, eParams,success,geom);
       this.driveWindowAnimation( actor, eParams, 0, action,success,geom);
+      
     }
 
     return;
@@ -130,14 +169,17 @@ const EffectsManager = new Lang.Class({
   animationDone : function (actor, action) {
 
     actor.hide();
+
     switch(action) {
       case "open" :
+        Main.wm._mapping.push(actor);
         Main.wm._mapWindowDone(this.shellwm ,actor);
         break;
       case "close" :
         Main.wm._destroyWindowDone(this.shellwm ,actor);
         return;
       case "minimize" :
+        Main.wm._minimizing.push(actor);
         Main.wm._minimizeWindowDone(this.shellwm ,actor);
         return;
       case "unminimize" :
@@ -146,6 +188,7 @@ const EffectsManager = new Lang.Class({
       case "other" :
       default:
     }
+
     actor.set_scale(1,1);
     actor.set_opacity(255);
     actor.set_pivot_point(0,0);
@@ -209,7 +252,7 @@ const EffectsManager = new Lang.Class({
     this.setNextParametersWindow(actor,eParams,subEffectNo*8+ 12,success,geom);
     actor.set_pivot_point( eParams[subEffectNo*8+ 9] ,eParams[subEffectNo*8+ 10] );
 
-      Tweener.addTween(actor, {
+    Tweener.addTween(actor, {
           opacity:           eParams[subEffectNo*8+ 11],
           x:                 eParams[subEffectNo*8+ 14],
           y:                 eParams[subEffectNo*8+ 15],
@@ -223,10 +266,59 @@ const EffectsManager = new Lang.Class({
           onOverwrite:       this.animationDone,
           onOverwriteScope : this,
           onOverwriteParams: [actor,action]
-      });
-      return;
+    });
+
+    return;
       
-   },
+  },
+   
+  extractEffect: function(effectList,startIndex,endIndex) {
+  
+    let eStr=[];
+  
+    while(startIndex <= endIndex) {
+      eStr.push(effectList[startIndex]);
+      startIndex++;
+    }
+    
+    return eStr;
+  
+  },
+
+  getEffectFor: function(appName,windowType,action) {
+    
+    let appIndex = (this.nameList.indexOf(appName)+1)*this.useApplicationProfiles;        
+    let effectIndex = 0;
+    let startIndex = 0;
+    
+    let endIndex = this.getEndIndex(this[windowType+"Window"+action+"Profile"],startIndex);
+   
+    while(startIndex!=-1) {
+    
+      if(effectIndex == appIndex) {
+        startIndex++;
+        return this.extractEffect(this[windowType+"Window"+action+"Profile"],startIndex,endIndex);
+      }
+      
+      effectIndex++;
+      startIndex = this[windowType+"Window"+action+"Profile"].indexOf('|',startIndex+1);
+      endIndex = this.getEndIndex(this[windowType+"Window"+action+"Profile"],startIndex);
+      
+    } 
+    
+  },
+  
+  getEndIndex: function(effectList,startIndex) {
+  
+    let endIndex = effectList.indexOf('|',startIndex+1);
+        
+    if(endIndex == -1) {
+      endIndex = effectList.length;
+    }  
+    
+    return --endIndex;
+
+  },
 
   initParametersOther: function(actor, eParams) {
 
@@ -263,13 +355,45 @@ const EffectsManager = new Lang.Class({
     this.onClosing       = null;
     this.onMinimizing    = null;
     this.onUnminimizing  = null;
-
+        
     this.openingWindowEffectEnabled = this.prefs.get_boolean("opening-effect-windows");
     this.openingOtherEffectEnabled  = this.prefs.get_boolean("opening-effect-others");
     this.closingingEffectEnabled    = this.prefs.get_boolean("closing-effect");
     this.minimizingEffectEnabled    = this.prefs.get_boolean("minimizing-effect");
     this.unMinimizingEffectEnabled  = this.prefs.get_boolean("unminimizing-effect");
+    
+    this.loadProfilePrefs();
 
+  },
+  
+  loadProfilePrefs: function() {
+  
+    this.useApplicationProfiles = this.prefs.get_boolean("use-application-profiles");
+    
+    this.nameList = this.prefs.get_strv("name-list");
+    
+    this.normalWindowopenProfile       = this.prefs.get_strv("normal-open");
+    this.normalWindowcloseProfile      = this.prefs.get_strv("normal-close");
+    this.normalWindowminimizeProfile   = this.prefs.get_strv("normal-minimize");
+    this.normalWindowunminimizeProfile = this.prefs.get_strv("normal-unminimize");    
+    
+    this.dialogWindowopenProfile       = this.prefs.get_strv("dialog-open");
+    this.dialogWindowcloseProfile      = this.prefs.get_strv("dialog-close");
+    this.dialogWindowminimizeProfile   = this.prefs.get_strv("dialog-minimize");
+    this.dialogWindowunminimizeProfile = this.prefs.get_strv("dialog-unminimize");    
+
+    this.modaldialogWindowopenProfile       = this.prefs.get_strv("modaldialog-open");
+    this.modaldialogWindowcloseProfile      = this.prefs.get_strv("modaldialog-close");
+    this.modaldialogWindowminimizeProfile   = this.prefs.get_strv("modaldialog-minimize");
+    this.modaldialogWindowunminimizeProfile = this.prefs.get_strv("modaldialog-unminimize");    
+
+    this.dropdownmenuWindowopenProfile       = this.prefs.get_strv("dropdownmenu-open");
+    this.popupmenuWindowopenProfile          = this.prefs.get_strv("popupmenu-open");
+    this.comboWindowopenProfile              = this.prefs.get_strv("combo-open");
+    this.splashscreenWindowopenProfile       = this.prefs.get_strv("splashscreen-open");
+    this.tooltipWindowopenProfile            = this.prefs.get_strv("tooltip-open");
+    this.overrideotherWindowopenProfile      = this.prefs.get_strv("overrideother-open");
+    
   },
 
   setNextParametersOther: function(actor,eParams,pIndex) {
@@ -295,14 +419,15 @@ const EffectsManager = new Lang.Class({
     let posChanged = true;
 
     if(eParams[pIndex]=="MW") {
-        eParams[pIndex] = (success) ? geom.width/actor.width : 0.1 ;
+        eParams[pIndex] = (success) ? geom.width/actor.width : (Main.layoutManager.monitors[global.display.get_current_monitor()].width*0.05)/actor.width ;
     }
 
     if(eParams[++pIndex]=="MH") {
-        eParams[pIndex] = (success) ? geom.height/actor.height : 0.1 ;
+        eParams[pIndex] = (success) ? geom.height/actor.height : (Main.layoutManager.monitors[global.display.get_current_monitor()].height*0.05)/actor.height ;
     }
 
     switch(eParams[++pIndex]) {
+    
       case "MX" :
         eParams[pIndex] = (success) ? geom.x:Main.layoutManager.monitors[global.display.get_current_monitor()].width/2;
         break;
@@ -323,10 +448,11 @@ const EffectsManager = new Lang.Class({
         break; 
       default:
         eParams[pIndex] = PX + parseFloat(eParams[pIndex]-1)*actor.width;
-        break;
+        
     }
 
     switch(eParams[++pIndex]) {
+    
       case "MY" :
         eParams[pIndex] = (success)? geom.y:Main.layoutManager.monitors[global.display.get_current_monitor()].height/2 ;
         break;
@@ -348,7 +474,7 @@ const EffectsManager = new Lang.Class({
 
       default :
         eParams[pIndex] = PY + parseFloat(eParams[pIndex]-1)*actor.height;
-         break;
+        
     }
 
     return [PX,PY,posChanged];
@@ -360,23 +486,23 @@ const EffectsManager = new Lang.Class({
     this.loadPreferences();
 
     if(this.openingWindowEffectEnabled) {
-        this.onOpening1 = global.window_manager.connect("map" , (shellwm ,actor ) => this.addWindowEffects ( shellwm, actor, "open" ));
+        this.onOpening1 = global.window_manager.connect("map", (shellwm ,actor ) => this.addWindowEffects ( shellwm, actor, "open" ));
     }
     if(this.openingOtherEffectEnabled) {
-        this.onOpening0 = global.display.connect("window-created" , (display ,window) => this.addOtherEffects ( window.get_compositor_private(), "open" ));
+        this.onOpening0 = global.display.connect("window-created", (display ,window) => this.addOtherEffects ( window.get_compositor_private(), "open" ));
     }
     if(this.closingingEffectEnabled) {
-        this.onClosing  = global.window_manager.connect("destroy" , (shellwm ,actor ) => this.addWindowEffects ( shellwm, actor, "close" ));
+        this.onClosing  = global.window_manager.connect("destroy", (shellwm ,actor ) => this.addWindowEffects ( shellwm, actor, "close" ));
     }
     if(this.minimizingEffectEnabled) {
-        this.onMinimizing  = global.window_manager.connect("minimize" , (shellwm ,actor ) => this.addWindowEffects ( shellwm ,actor ,"minimize" ));
+        this.onMinimizing  = global.window_manager.connect("minimize", (shellwm ,actor ) => this.addWindowEffects ( shellwm ,actor ,"minimize" ));
     }
     if(this.unMinimizingEffectEnabled) {
-        this.onUnminimizing  = global.window_manager.connect("unminimize" , (shellwm ,actor ) => this.addWindowEffects ( shellwm ,actor ,"unminimize" ));
+        this.onUnminimizing  = global.window_manager.connect("unminimize", (shellwm ,actor ) => this.addWindowEffects ( shellwm ,actor ,"unminimize" ));
     }
-
+  
   },
-
+    
   destroy: function () {
 
     if(this.openingWindowEffectEnabled) {
@@ -398,3 +524,6 @@ const EffectsManager = new Lang.Class({
   },
 
 });
+
+
+
